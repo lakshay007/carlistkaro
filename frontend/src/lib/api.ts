dffee6af2fc25356f19e9192f430adb0612813a4
Lakshay <dmslakshay@gmail.com>
2024-11-14 14:43:55 +0530

    api url update

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:9000/api';
+const API_URL = 'https://carlistkaro.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
